cddf88ed348ecf2125877c8120d7033104a682f3
Longress <37417429+Longress@users.noreply.github.com>
2018-03-16 01:28:13 +0300

    OneMoreLife

---
+++ bot.py
@@ -0,0 +1,16 @@
+﻿import telebot
+
+TOKEN = '577132625:AAER-D3-4zRIQro-y3Mo8gxZeGlDRpI4DO0'
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start_handler(message):
+    bot.send_message(message.chat.id, 'Ну здравствуй, молодой пидорас, какое у тебя имя?')
+
+@bot.message_handler(content_types=['text'])
+def text_handler(message):
+    text = message.text.lower()
+    chat_id = message.chat.id
+    bot.send_message(chat_id, 'Мне вообще ткак то на тебя похуй, иди сосать')
+
+bot.polling()
